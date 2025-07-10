afbd512d8aa619ba7563a33fda9b628d5f58c624
Andrii Poliukh <andriypoliukh@gmail.com>
2025-07-10 20:02:52 +0300

    add error

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -25,6 +25,8 @@ function transformStateWithClones(state, actions) {
           delete stateCopy[key];
         }
         break;
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
     result.push({ ...stateCopy });
   }
